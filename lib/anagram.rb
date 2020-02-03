df3fd3aeb210463e3173566eb040ed22e532664d
Adam G Horn <hornadam8@gmail.com>
2020-02-03 19:38:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-class
+class Anagram
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    mat = []
+    array.each do |word|
+      if word.split.sort == @word.split.sort
+        mat << word
+      end
+    end
+    mat
+  end
+  
+  
+  
+end
